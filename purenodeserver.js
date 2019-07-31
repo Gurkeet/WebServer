1cc3e09bf1db757b9a10f7b83fc6b4f9c99b59c2
Gurkeet Puri <748159@student.tor.loc>
2019-07-31 14:50:11 -0400

    first commit

---
+++ purenodeserver.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+function handler(req, res) {
+    res.writeHead(200, { 'Content-type': 'text/plain' });
+    res.write('Hello, I am a web server');
+    res.end();
+}
+const server = http.createServer(handler);
+
+server.listen(3000);
