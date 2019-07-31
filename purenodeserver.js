a45d07abbddcbfa9fa0f952e912c7835c39b2460
Gurkeet Puri <748159@student.tor.loc>
2019-07-31 14:59:02 -0400

    second commit

--- purenodeserver.js
+++ purenodeserver.js
@@ -1,6 +1,8 @@
 const http = require('http');
 
 function handler(req, res) {
+
+    const parsedUrl = url.parse(req.url, true);
     res.writeHead(200, { 'Content-type': 'text/plain' });
     res.write('Hello, I am a web server');
     res.end();
